9e0f9b895c624b59835fd80002a364f47b6ff6fc
Ezuvi-149 <daniappgamer@gmail.com>
2025-01-22 00:17:29 +0100

    Add files via upload

--- seat_switcher/README.md
+++ seat_switcher/README.md
@@ -1,8 +1,5 @@
 # Scripts-FiveM-QB
 Scripts Sencillos para FiveM con el Framework de QB
 
-# Video de Muestra (click en la imagen)
 
-[![IMAGE ALT TEXT HERE](https://i.gyazo.com/29332ac4c1848012115ae31eddb30c03.jpg)](https://youtu.be/Mef7oBEFjAI)
-
-- https://github.com/Ezuvi-149/Scripts-FiveM-QB/tree/main
+https://github.com/Ezuvi-149/Scripts-FiveM-QB/tree/main
--- seat_switcher/client.lua
+++ seat_switcher/client.lua
@@ -6,34 +6,49 @@ local function OpenSeatMenu()
     
     if not DoesEntityExist(vehicle) then return end
     
-    local isBike = IsThisModelABike(GetEntityModel(vehicle))
+    local vehicleModel = GetEntityModel(vehicle)
+    local maxSeats = GetVehicleModelNumberOfSeats(vehicleModel)
     
-    local elements = {}
+    local menuItems = {
+        {
+            header = "Cambiar de asiento",
+            isMenuHeader = true
+        }
+    }
     
-    if isBike then
-        elements = {
-            {header = "Conductor", params = {event = "seat_switcher:changeSeat", args = {seat = -1}}},
-            {header = "Pasajero", params = {event = "seat_switcher:changeSeat", args = {seat = 0}}}
-        }
-    else
-        elements = {
-            {header = "Conductor", params = {event = "seat_switcher:changeSeat", args = {seat = -1}}},
-            {header = "Copiloto", params = {event = "seat_switcher:changeSeat", args = {seat = 0}}},
-            {header = "Asiento trasero izquierdo", params = {event = "seat_switcher:changeSeat", args = {seat = 1}}},
-            {header = "Asiento trasero derecho", params = {event = "seat_switcher:changeSeat", args = {seat = 2}}}
-        }
+    for i = -1, maxSeats - 2 do
+        local seatName
+        if i == -1 then
+            seatName = "Conductor"
+        elseif i == 0 then
+            seatName = "Copiloto"
+        else
+            seatName = "Asiento " .. tostring(i + 1)
+        end
+        
+        if IsVehicleSeatFree(vehicle, i) then
+            table.insert(menuItems, {
+                header = seatName,
+                params = {
+                    event = "qb-seatswitcher:switchSeat",
+                    args = {
+                        seatIndex = i
+                    }
+                }
+            })
+        end
     end
     
-    exports['qb-menu']:openMenu(elements)
+    exports['qb-menu']:openMenu(menuItems)
 end
 
-RegisterNetEvent('seat_switcher:changeSeat')
-AddEventHandler('seat_switcher:changeSeat', function(data)
+RegisterNetEvent('qb-seatswitcher:switchSeat')
+AddEventHandler('qb-seatswitcher:switchSeat', function(data)
     local playerPed = PlayerPedId()
     local vehicle = GetVehiclePedIsIn(playerPed, false)
     
-    if DoesEntityExist(vehicle) then
-        SetPedIntoVehicle(playerPed, vehicle, data.seat)
+    if DoesEntityExist(vehicle) and data.seatIndex ~= nil then
+        SetPedIntoVehicle(playerPed, vehicle, data.seatIndex)
     end
 end)
 
@@ -41,4 +56,4 @@ RegisterCommand('cambiarasiento', function()
     OpenSeatMenu()
 end, false)
 
-RegisterKeyMapping('cambiarasiento', 'Abrir menú de cambio de asiento', 'keyboard', 'K')
+RegisterKeyMapping('cambiarasiento', 'Abrir menú de cambio de asiento', 'keyboard', '§')
